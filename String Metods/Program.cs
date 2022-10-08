efbd90f819f613263e93f5d6487286722a9ef28c
naslanzade <novrastafa@code.edu.az>
2022-10-08 21:11:38 +0400

    add String metods folder

---
+++ String Metods/Program.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Text;
+
+namespace String_Metods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Homework
+
+
+            //int a = 5;
+            //int b = a;
+            //Console.WriteLine(a + " " + b);
+
+
+            //int a = 5;
+            //int b = a;
+            //Console.WriteLine(a + "---" +b);
+
+
+            //int a = 5;
+            //int b = a;
+            //b = 18;
+            //Console.WriteLine(a + "----" +b);
+
+
+            //int a = 5;
+            //int b = a;
+            //b = 18;
+            //Console.WriteLine(a + " " +b);
+
+
+            //int[] numbers1 = { 1, 3, 4, 5, 6, 7, 8, 9 };
+            //int[] numbers2 = numbers1;
+            //Console.WriteLine(numbers1[0] + "  "+ numbers2[0]);
+
+
+
+            //int[] numbers1 = { 1, 3, 4, 5, 6, 7, 8, 9 };
+            //int[] numbers2 = numbers1;
+            //numbers2[0] = 99;
+            //Console.WriteLine(numbers1[0] + "  " + numbers2[0]);
+
+            //int num = 13;
+            //ShowNum(num);
+            //Console.WriteLine(num);
+
+
+            //int number = 13;
+            //Console.WriteLine(number);
+
+            //number += 3;
+            //ShowNum(number);
+            //Console.WriteLine(number);
+
+
+            //int numb = 13;
+            //ShowNum(ref numb);
+            //Console.WriteLine(numb);
+
+
+
+            //int j = 13;
+            //j += 5;
+            //ShowNumber(ref j);
+            //Console.WriteLine(j);
+
+
+
+            //int k;
+            //ShowNums(out k);
+            //Console.WriteLine(k);
+
+
+            //int m;
+            //ShowNumbers(out m);
+            //Console.WriteLine(m);
+
+
+            //int n;
+            //ShowNumber(out n);
+            //n += 2;
+            //Console.WriteLine(n);
+
+
+            //string name1 = "Qoshqar";
+            //name1 = "Ramil";
+            //Console.WriteLine(name1);
+
+
+            //string name1 = "Qoshqar";
+            //string name2 = name1;
+            //name2 = "Cavid";
+            //Console.WriteLine(name1);
+            //Console.WriteLine(name2);
+
+
+
+            //string name1 = "Qoshqar";
+            //CheckStr(name1);
+            //Console.WriteLine(name1);
+
+
+            //string name1 = "Qoshqar";
+            //CheckStr(ref name1);
+            //Console.WriteLine(name1);
+
+
+
+            //string name1;
+            //CheckStr(out name1);
+            //Console.WriteLine(name1);
+
+
+
+            //StringBuilder str = new StringBuilder();
+            //str.Append("Cav");
+            //str.Append("id");
+            //Console.WriteLine(str);
+
+
+            //string str1 = "Cav";
+            //string str2 = "id";
+            //string name = str1 + str2;
+            //Console.WriteLine(name);
+
+
+            //String name1 = new string("Lale");
+            //Console.WriteLine(name1);
+
+
+            //string name = "Ramil";
+            //Console.WriteLine(name.Length);
+
+
+            //string name = "Ramil";
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+
+            //string name = "     Ramil";
+            //Console.WriteLine(name.Trim());
+
+
+            //string name = "Ramil";
+            //name = String.Empty;
+            //Console.WriteLine(name);
+
+
+            //string name = "Ramil";
+            //Console.WriteLine(name.StartsWith("r"));
+
+
+            //string name = "Ramil";
+            //Console.WriteLine(name.StartsWith("R"));
+
+
+            //string name = "Ramil Azer";
+            //Console.WriteLine(name.StartsWith("R"));
+
+
+            //string name = "Ramil Azer";
+            //Console.WriteLine(name.StartsWith("Rm"));
+
+
+            //string name = "Ramil";
+            //Console.WriteLine(name.EndsWith("R"));
+
+
+            //string name = "Ramil";
+            //Console.WriteLine(name.EndsWith("l"));
+
+
+            //string name = "Ramil";
+            //if (name.StartsWith("R"))
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+
+            //string name = "RAMIL";
+            //Console.WriteLine(name.ToLower());
+
+
+            //string name = "ramil";
+            //Console.WriteLine(name.ToUpper());
+
+
+            //string name = "Ramil";
+
+            //if (name.ToLower().StartsWith("R"))
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+
+            //string name = "Ramil";
+            //Console.WriteLine(name.IndexOf("a"));
+
+
+            //string name = "Ramil";
+            //Console.WriteLine(name.Replace("R","K"));
+
+            //string name = "Hello Ramil";
+            //Console.WriteLine(name.Remove(3,7));
+
+
+            //string name = "Hello Ramil";
+            //Console.WriteLine(name.Substring(5));
+
+
+            //string str = "Hello world";
+            //var result = str.Split(" ");
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //string str = "robot";
+            //var result = str.ToCharArray();
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //string[] words = { "one", "two", "three" };
+            //Console.WriteLine(string.Join(",",words));
+
+
+            //string text = "Test";
+            //if (string.IsNullOrEmpty(text))
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+            //string text = "Hello";
+            //string text1 = "World";
+            //Console.WriteLine(string.Concat(text, "World"));
+
+
+            //string fullname = "Novrasta Aslanzade";
+            //Console.WriteLine(fullname.Insert(0,"Miss "));
+
+
+            //string firstname = "Novrasta";
+            //string lastname = "Aslanzade";
+            //Console.WriteLine(firstname.CompareTo(lastname));
+
+
+            //string firstname = "Nana";
+            //Console.WriteLine(firstname.GetHashCode());
+
+
+
+            //string a = "Hello";
+            //string b = "World";
+            //Console.WriteLine(a.Equals(b));
+
+
+        }
+
+
+        //public static void ShowNum(int num)
+        //{
+        //    num += 100;
+        //    Console.WriteLine(num);
+        //}
+
+
+
+        //public static void ShowNum(int number)
+        //{
+        //    number += 100;
+        //    Console.WriteLine(number);
+        //}
+
+
+
+        //public static void ShowNum(ref int numb)
+        //{
+        //    numb += 100;
+        //    Console.WriteLine(numb);
+        //}
+
+
+        //public static void ShowNumber(ref int j)
+        //{
+        //    j += 100;
+        //    Console.WriteLine(j);
+        //}
+
+
+        //public static void ShowNums(out int k)
+        //{
+        //    k = 100;
+        //    Console.WriteLine(k);
+        //}
+
+
+        //public static void ShowNumbers(out int m)
+        //{
+        //    m = 100;
+        //    m += 2;
+        //    Console.WriteLine(m);
+        //}
+
+
+        //public static void ShowNumber(out int n)
+        //{
+        //    n = 100;
+        //    n += 2;
+        //    Console.WriteLine(n);
+        //}
+
+
+        //public static void CheckStr(string str)
+        //{
+        //    str = "Mahir";
+        //    Console.WriteLine(str);
+        //}
+
+
+
+        //public static void CheckStr(ref string str)
+        //{
+        //    str = "Mahir";
+        //    Console.WriteLine(str);
+        //}
+
+
+        //public static void CheckStr(out string str)
+        //{
+        //    str = "Mahir";
+        //    str += "-Cavid";
+        //    Console.WriteLine(str);
+        //}
+
+
+
+
+
+
+       
+
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #endregion
+
+
+
+    }
+
